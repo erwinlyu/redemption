da6af02f75aa22f9f3d1ef7f5f9872186aa788b6
erwinlyu <erwinlu2011@gmail.com>
2019-11-17 14:01:32 +0800

    medium-update:manacher's algorithm

--- lc5-longestPalindromicSubstring.cpp
+++ lc5-longestPalindromicSubstring.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.empty())   return "";
+        // brute force 
+        // (check each point as the center point or between the center pair of a palindrome)
+/*         if(s.empty())   return "";
         if(s.length() == 1) return s;
         int maxLen = 0, start;
         for(int i = 0; i < s.length() - 1; i++){
@@ -42,7 +46,69 @@ public:
                 }
             }
         }
-        return s.substr(start, maxLen);
+        return s.substr(start, maxLen); */
+
+
+        //Manacher's algorithm  O(n) O(n)
+        //much faster than brute force (12ms vs 28ms)
+        //little bit more memory than brute force(10.1MB vs 8.9MB)
+
+        //refrences:
+        //https://medium.com/hackernoon/manachers-algorithm-explained-longest-palindromic-substring-22cb27a5e96f
+        //https://blog.csdn.net/HappyRocking/article/details/82622881
+
+        if(s.empty())   return "";
+        if(s.length() == 1) return s;
+        string oddenized = oddenize(s);
+        int slength = oddenized.length();
+        int maxLen = 0, position = 0;
+        vector<int> m;
+        m.resize(slength);
+        int c = 0, r = 0;
+        for(int i = 1; i < slength; i++){
+            int mirror = 2 * c - i;
+            //the core idea of manacher's algorithm, determine the minumum certain possible
+            //expansion distance of each node
+            if(i < r){
+                m[i] = min(m[mirror], r - i);
+            }
+
+            //try to expand the expansion of node i
+            int templ = i - (m[i] + 1);
+            int tempr = i + (m[i] + 1);
+            while(templ >= 0 && tempr < slength && oddenized[templ] == oddenized[tempr]){
+                m[i]++;
+                templ--;
+                tempr++;
+            }
+
+            //if i expands beyond the current right boundry, update center and right
+            //as well as the maxlen of palindrome
+            if(i + m[i] > r){
+                c = i;
+                r = i + m[i];
+                if(m[i] > maxLen){
+                    maxLen = m[i];
+                    position = i;
+                }
+            }
+        }
+        string sub = oddenized.substr(position - maxLen, 2 * maxLen + 1);
+        int index;
+        while((index = sub.find('#')) != string::npos){
+            sub.erase(index, 1);
+        }
+        return sub;
+    }
+
+    string oddenize(string s){
+        string str;
+        for(int i = 0; i < s.length(); i++){
+            str += "#";
+            str += s[i];
+        }
+        str.append("#");
+        return str;
     }
 };
 
