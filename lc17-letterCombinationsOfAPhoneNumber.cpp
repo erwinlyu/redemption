e69d0a80aeefbfa290a1e4d65b57d60e6ffcd097
erwinlyu <erwinlu2011@gmail.com>
2019-11-22 17:20:42 +0800

    medium-dfs, update:a little modification, improve performance

--- lc17-letterCombinationsOfAPhoneNumber.cpp
+++ lc17-letterCombinationsOfAPhoneNumber.cpp
@@ -16,27 +16,31 @@ public:
         return vofs;
     }
     void dfs(int depth, int index, string digits, string str){
-        cout << "current depth " << depth << endl;
-        if(depth == digits.size() - 1){
+        // little modification to the termination condition of dfs over the code below
+        // which reduced rumtime significantly (0ms vs 8ms)
+        // and saved a little memory (8.4MB vs 8.8MB)
+        // both of them are currently state-of=the-art performance in leetcode
+        if(depth == digits.length()){
+            vofs.push_back(str);
+            return ;
+        }
+/*         if(depth == digits.size() - 1){
             while(index < lut[digits[depth] - '2'].size()){
-                cout << "current index is " << index << endl;
                 str += lut[digits[depth] - '2'][index];
                 vofs.push_back(str);
                 str.pop_back();
                 index++;
             }
             return ;
+        } */
+
+        while(index < lut[digits[depth] - '2'].size()){
+            str += lut[digits[depth] - '2'][index];
+            dfs(depth + 1, 0, digits, str);
+            str.pop_back();
+            index++;
         }
-        else{
-            while(index < lut[digits[depth] - '2'].size()){
-                cout << "current index is " << index << endl;
-                str += lut[digits[depth] - '2'][index];
-                dfs(depth + 1, 0, digits, str);
-                str.pop_back();
-                index++;
-            }
-            return ;
-        }
+        return ;
     }
 };
 int main(){
