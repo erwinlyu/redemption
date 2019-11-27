d025414b946eb2f7b2858060e9f8ff200c40ca8c
erwinlyu <erwinlu2011@gmail.com>
2019-11-27 20:31:59 +0800

    easy-exam the range of int32. Update: a succinct method

--- lc7-reverseInteger.cpp
+++ lc7-reverseInteger.cpp
@@ -31,6 +31,24 @@ public:
     }
 };
 
+/* CORRECT ANSWER
+//actually no need to convert a negative number into positve in the first place
+//e.g.  -129 % 10 gives -9
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}; */
+
 int main(){
     int input;
     cin >> input;
