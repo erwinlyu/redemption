09447e6afb6b6da2a7a605f92727632d8e202589
erwinlyu <erwinlu2011@gmail.com>
2019-12-06 16:39:20 +0800

    medium-use <set>, update: fancier, self-explaining method

--- lc36-validSudoku.cpp
+++ lc36-validSudoku.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        set<char> blocks[3][3];
+/*         set<char> blocks[3][3];
         set<char> rows[9];
         set<char> cols[9];
         for(int i = 0; i < 9; i++){
@@ -18,6 +18,24 @@ public:
                 else    cols[j].insert(temp);
                 if(blocks[i / 3][j / 3].find(temp) != blocks[i / 3][j / 3].end())   return false;
                 else    blocks[i / 3][j / 3].insert(temp);
+            }
+        }
+        return true; */
+
+        // one set
+        // seem to pass on VS 2019 compiler but leetcode runtime error
+        set<string> exists;
+        char int2char[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                char temp = board[i][j];
+                if(temp == '.')  continue;
+                if(exists.find(temp + "in row" + int2char[i]) != exists.end())    return false;
+                else    exists.insert(temp + "in row" + int2char[i]);
+                if(exists.find(temp + "in col" + int2char[j]) != exists.end())  return false;
+                else    exists.insert(temp + "in col" + int2char[j]);
+                if(exists.find(temp + "in block" + int2char[i / 3] + int2char[j / 3]) != exists.end())  return false;
+                else    exists.insert(temp + "in block" + int2char[i / 3] + int2char[j / 3]);
             }
         }
         return true;
