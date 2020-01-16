117dd17990e180f1dd58932d9ee77fc5866a3dc7
erwinlyu <erwinlu2011@gmail.com>
2020-01-16 16:17:36 +0800

    RIA-medium-learnt a fancy method to this kind of problem

---
+++ lc80-removeDuplicatesFromSortedArrayII.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    // reference:https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/discuss/27976/3-6-easy-lines-C%2B%2B-Java-Python-Ruby
+    // this method can be generalized to k-most duplicates problem.
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for (n : nums) {
+            if (i < 2 || n > nums[i - 2]) {
+                nums[i++] = n;
+            }
+        }
+        return i;
+    }
+};
