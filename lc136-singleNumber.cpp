91abfa29417df7c48da2a0c2749cbd456fbb39a8
erwinlyu <erwinlu2011@gmail.com>
2020-03-06 18:29:19 +0800

    MARK-easy-xor bit operation

---
+++ lc136-singleNumber.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for (int i : nums)  res ^= i;
+        return res;
+    }
+};
